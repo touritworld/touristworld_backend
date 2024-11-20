bfa27e87e70513564bd16f0929cfa66a66106274
propiter <pro.pedro0413@gmail.com>
2024-11-19 19:51:49 -0500

    .dev

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,44 +1,51 @@
 import pkg from 'pg';
+const { Pool } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { Client } = pkg;
+const dbConfig = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } } // Configuración para Vercel
+  : {
+      user: process.env.DB_USER, // Configuración local
+      host: process.env.DB_SERVER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT, 10),
+    };
 
-const dbConfig = {
-  user: process.env.DB_USER,
-  host: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10), // Aseguramos el número entero
-};
+let pool = null;
 
-let client = null;
-
+// Conexión a PostgreSQL
 export async function connectPostgres() {
-  if (client) {
-    return client; // Reutilizamos la conexión si ya está establecida
+  if (pool) {
+    return pool; // Reutilizamos la conexión si ya está establecida
   }
 
-  client = new Client(dbConfig);
+  pool = new Pool(dbConfig); // Usamos Pool para gestionar conexiones
 
   try {
-    await client.connect();
-    console.log('Conectado a PostgreSQL');
-    return client;
+    await pool.query('SELECT 1'); // Prueba básica para validar la conexión
+    console.log(
+      process.env.DATABASE_URL
+        ? 'Conectado a PostgreSQL en Vercel'
+        : 'Conectado a PostgreSQL local'
+    );
+    return pool;
   } catch (error) {
     console.error('Error de conexión a PostgreSQL:', error);
-    client = null; // Reiniciamos el cliente si falla
+    pool = null; // Reiniciamos el pool si falla
     throw error;
   }
 }
 
+// Desconectar PostgreSQL
 export async function disconnectPostgres() {
-  if (client) {
+  if (pool) {
     try {
-      await client.end();
+      await pool.end();
       console.log('Desconexión de PostgreSQL exitosa');
-      client = null;
+      pool = null;
     } catch (error) {
       console.error('Error al desconectar de PostgreSQL:', error);
       throw error;
